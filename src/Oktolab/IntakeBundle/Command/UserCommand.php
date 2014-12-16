9bc865a4b80bff80e87692adcd6f04ac584cf0a4
rs <rs@okto.tv>
2014-12-16 19:42:56 +0100

    fixed usercommand task

--- src/Oktolab/IntakeBundle/Command/UserCommand.php
+++ src/Oktolab/IntakeBundle/Command/UserCommand.php
@@ -1,14 +1,17 @@
 <?php
 
+namespace Oktolab\IntakeBundle\Command;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use Oktolab\IntakeBundle\Entity\User;
+use Oktolab\IntakeBundle\Entity\User as User;
 
 //TODO: remove the password option as it is not save to have it written in the commandline
 // set it blank and use the password 'reset' option and send it via email
+
 class UserCommand extends Command
 {
     /**
@@ -22,7 +25,6 @@ class UserCommand extends Command
             ->addOption('name', '-n', InputOption::VALUE_REQUIRED, 'name of the account')
             ->addOption('email', '-e', InputOption::VALUE_REQUIRED, 'email of the account')
             ->addOption('password', '-p', InputOption::VALUE_REQUIRED, 'password of the account')
-
         ;
     }
 
@@ -31,7 +33,7 @@ class UserCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        User $user = new User();
+        $user = new User();
         $user->setName($input->getOption('name'));
         $user->setEmail($input->getOption('email'));
         $user->setPassword($input->getOption('password'));
