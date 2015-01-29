589036af0ba302f4df64a6711e0f9e7d5140235e
rs <rs@okto.tv>
2015-01-29 17:43:02 +0100

    implemented filecap

---
+++ src/Oktolab/IntakeBundle/Form/ConfigType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oktolab\IntakeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ConfigType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('configKey')
+            ->add('configValue')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Oktolab\IntakeBundle\Entity\Config'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'oktolab_intakebundle_config';
+    }
+}
